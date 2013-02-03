44d81a994b8f6833c96563cee34bcea51d2c1cfd
Matthew Bush <bushmat4@gmail.com>
2013-02-02 22:05:07 -0500

    added scroll to top button

--- js/plugins.js
+++ js/plugins.js
@@ -13,4 +13,29 @@ if (!(window.console && console.log)) {
 
 // Place any jQuery/helper plugins in here.
 
+$(document).ready(function(){
+
+	// hide #back-top first
+	$("#back-top").hide();
+	
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+});
   
